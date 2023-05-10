736df401e4ed3eec987b07f2d10249c8d6d2bde9
Ali Raza <aliraza021419@gmail.com>
2023-05-10 15:57:58 -0400

    Added javascript functionality to confirm if password matches

---
+++ script.js
@@ -0,0 +1,19 @@
+// confirm if password matches //
+const password = document.querySelector('input[name=password]');
+const confirmPassword = document.querySelector('input[name=confirm-password]');
+const errorMessage = document.querySelector('.error-message');
+
+password.oninput = () => checkPassword();
+confirmPassword.oninput = () => checkPassword();
+
+function checkPassword() {
+    if (password.value != confirmPassword.value) {
+        password.classList.add("error");
+        confirmPassword.classList.add("error");
+        errorMessage.textContent = "*Passwords do not match";
+    } else {
+        password.classList.remove("error");
+        confirmPassword.classList.remove("error");
+        errorMessage.textContent = "";
+    }
+}
