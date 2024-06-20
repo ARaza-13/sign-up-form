f32b2104780fe5cc4d1ef6734f3b9d4449c23551
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-06-19 21:49:08 -0400

    Set up an event listener to handle input events for whenever the user types in an input field

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('sign-up-form');
+
+    // check if the form field is valid whenever the user types something
+    form.addEventListener('input', function (event) {
+        const target = event.target;
+        if (target.tagName === 'INPUT') {
+            validateField(target);
+        }
+    });
+
+    function validateField(field) {
+        console.log(field);
+    }
+});
+
 const password = document.querySelector('input[name=password]');
 const confirmPassword = document.querySelector('input[name=confirm-password]');
 const errorMessage = document.getElementById('confirm-password-error');
