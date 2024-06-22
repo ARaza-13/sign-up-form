c1c9608bfef5150f8fcc3ac23b4d3afe189a4285
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-06-22 00:25:02 -0400

    Added a function that checks if the password meets every requirement. If not, the error message will actively update as the user inputs to inform which requirements have been met and which requirements have not.

--- script.js
+++ script.js
@@ -32,6 +32,8 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (validity.patternMismatch) {
             if (field.id === 'phone') {
                 errorMessage = 'Enter a valid phone number (xxx-xxx-xxxx).';
+            } else if (field.id = 'password') {
+                errorMessage = getPasswordErrorMessage(field.value);
             }
         } else if (validity.typeMismatch) {
             if (field.type === 'email') {
@@ -41,6 +43,29 @@ document.addEventListener('DOMContentLoaded', function () {
             if (field.type === 'email') {
                 errorMessage = `Email should be at least ${field.minLength} characters. You entered ${field.value.length}.`
             }
+        }
+
+        return errorMessage;
+    }
+
+    function getPasswordErrorMessage(password) {
+        let errorMessage = 'Password must have:';
+        const requirements = [
+            { regex: /[A-Z]/, message: 'a capital letter' },
+            { regex: /\d/, message: 'a number' },
+            { regex: /[@$!%*?&]/, message: 'a special character' },
+            { regex: /.{8,}/, message: 'at least 8 characters' },
+        ];
+
+        // test the password against each regular expression 
+        // add requirement the the 'errors' array if requirement not met
+        // transform array of unmet requirements into array of error messages
+        const errors = requirements.filter(req => !req.regex.test(password)).map(req => req.message);
+
+        if (errors.length > 0) {
+            errorMessage += ' ' + errors.join(', ') + '.';
+        } else {
+            errorMessage = '';
         }
 
         return errorMessage;
